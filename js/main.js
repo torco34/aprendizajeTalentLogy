606a71e8b5084a4930fa8f4d9ce993f19644c150
torcoroma <torcoromaarias@gmail.com>
2023-03-01 15:49:47 -0500

    corregir errores

--- js/main.js
+++ js/main.js
@@ -2,27 +2,18 @@
 const element = document.getElementById("element");
 
 console.log(element);
-// let addNumber = prompt("Introduce numero");
+let addNumber = prompt("Introduce numero");
 
-// element.innerHTML = `Has introducido el número ${addNumber} Gracias`;
+element.innerHTML = `Has introducido el número ${addNumber} Gracias`;
 
-//segundo ejercicio
+//Segundo ejercicio
 
 const elementDos = document.getElementById("elementDos");
 
-let precio = prompt("Indrudusca el precio");
-let descuento = prompt("Indrudusca el descuento");
+let precio = prompt("Indruduzca el precio");
+let descuento = prompt("Indruduzca el descuento");
 
 const descuentoTotal = Math.abs((precio * descuento) / 100 - precio);
 
 console.log(descuentoTotal);
-elementDos.innerHTML = `El precio es ${precio} tiene el descuento de ${descuento} el total del precio es ${descuentoTotal}`;
-
-
-// function descuento(numero, porsentaje) {
-//   return Math.abs((numero * porsentaje) / 100 - numero);
-// }
-
-// const resultado = descuento(100, 35);
-
-// console.log(`resultado ${resultado}`);
+elementDos.innerHTML = `El precio es de ${precio} y tiene el descuento de ${descuento} el total del precio es de${descuentoTotal}`;
