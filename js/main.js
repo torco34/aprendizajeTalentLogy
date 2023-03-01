434e8528a863b9f1cc50930845257a8e28aa3f35
torcoroma <torcoromaarias@gmail.com>
2023-03-01 14:52:27 -0500

    Primer ejercicio

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const element = document.getElementById("element");
+
+console.log(element);
+let addNumber = prompt("introduce numero");
+
+element.innerHTML = `Has introducido el número ${addNumber} Gracias`;
