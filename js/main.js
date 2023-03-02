f0d822870f1e781a94f32ce7158925eb5c709f16
torcoroma <torcoromaarias@gmail.com>
2023-03-02 08:54:28 -0500

    Ejercicios en input

--- js/main.js
+++ js/main.js
@@ -1,47 +1,47 @@
-// Primer  ejercicio
-// const element = document.getElementById("element");
+// DESARROLLO DEL PRIMER EJERCICIO
+const inputNumber = document.getElementById("inputNumber");
+const element = document.getElementById("element");
+const btnNumber = document.getElementById("btnNumber");
+function viewNumber(e) {
+  e.preventDefault();
+  let value = inputNumber.value;
 
-// console.log(element);
-// let addNumber = prompt("Introduce numero");
+  element.innerHTML = `Has intruducido el numero ${value} Gracias`;
+}
+btnNumber.addEventListener("click", viewNumber);
 
-// element.innerHTML = `Has introducido el número ${addNumber} Gracias`;
+// SEGUNDO EJERCICIO
+const inputProducts = document.getElementById("products");
+const inputDiscount = document.getElementById("discount");
+const elementDos = document.getElementById("elementDos");
+const btn = document.getElementById("btn");
+console.log(inputProducts);
+function discountPrice(e) {
+  e.preventDefault();
+  let valueProduct = inputProducts.value;
+  let discountProduct = inputDiscount.value;
+  const preciTatal = Math.abs(
+    (valueProduct * discountProduct) / 100 - valueProduct
+  );
+  elementDos.innerHTML = `El precio es de 
+  ${valueProduct} tiene un descuento de 
+  ${discountProduct}%  el precio final con el descuento 
+  ${preciTotal}`;
+}
 
-//Segundo ejercicio
-
-// const elementDos = document.getElementById("elementDos");
-
-// let precio = prompt("Indruduzca el precio");
-// let descuento = prompt("Indruduzca el descuento");
-
-// const descuentoTotal = Math.abs((precio * descuento) / 100 - precio);
-
-// elementDos.innerHTML = `El precio es de ${precio} y tiene el descuento de ${descuento} el total del precio es de ${descuentoTotal}`;
-
-// // el mismo ejercicio con function
-
-// function descuento(numero, porsentaje) {
-//   return Math.abs((numero * porsentaje) / 100 - numero);
-// }
-
-// const resultado = descuento(100, 35);
-
-// console.log(`resultado ${resultado}`);
+btn.addEventListener("click", discountPrice);
 
 // CUARTO EJERCICIO
 
-const botton = document.getElementById("button");
+const button = document.getElementById("button");
 const input = document.getElementById("input");
 const inputTotal = document.getElementById("inputTotal");
-console.log(input);
 
-function saberPrecio(e) {
+function calculateChange(e) {
   e.preventDefault();
-
-  const dolar = input.value * 1;
-
-  const volores = 1.3325 * dolar;
-
-  console.log(`dolar ${volores}`);
+  let dollar = input.value * 1;
+  let values = 1.3325 * dollar;
+  console.log(`dolar ${values}`);
 }
 
-botton.addEventListener("click", saberPrecio);
+button.addEventListener("click", calculateChange);
