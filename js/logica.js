c739e1ef0a31daed1021b1fd5be6d83920f6a53c
torcoroma <torcoromaarias@gmail.com>
2023-03-06 16:24:21 -0500

    Tercer ejerccio de logica

--- js/logica.js
+++ js/logica.js
@@ -21,15 +21,34 @@ btnOne.addEventListener("click", pedirNumero);
 
 //  SEGUNDO EJERCICIO DE LOGICA
 const celsius = 32;
-function converFahrenheitACelsius(celsius) {
+function convertirFahrenheitACelsius(celsius) {
   let resultado = (celsius * 9) / 5.0 + 32;
   return resultado;
 }
-function convertCelsiusAFahrenheit(fahrenheit) {
+function convertirCelsiusAFahrenheit(fahrenheit) {
   let resultado = fahrenheit + (32 / 5.0) * 9;
   return resultado;
 }
 
-let resultado = convertCelsiusAFahrenheit(celsius);
+let resultado = convertirCelsiusAFahrenheit(celsius);
 
-console.log(`resultado ${celsius} 0C  = ${resultado} 0F`);
+console.log(`resultado de ${celsius} 0C es = ${resultado} 0F`);
+
+// TERCER EJERCICIO DE LOGICA
+
+// Sacar la hipotenusa de un triángulo rectángulo, pidiendo al usuario el valor de los 2 catetos.
+const catetos = document.getElementById("catetos");
+function CalcularHipotenusa() {
+  let cateto1 = prompt("Ingresa cateto 1");
+  let cateto2 = prompt("Ingresa catetos 2");
+  let a = Math.pow(cateto1, 2);
+  let b = Math.pow(cateto2, 2);
+  suma = a + b;
+
+  resultado = Math.sqrt(Math.pow(cateto1, 2) + Math.pow(cateto2, 2));
+  console.log(
+    `Resultado: de numeros ingresados ${cateto1} y ${cateto2}, resultado elevados  es ${a} y ${b},  la hiputenusa de ${suma} es ${resultado}`
+  );
+}
+
+catetos.addEventListener("click", CalcularHipotenusa);
