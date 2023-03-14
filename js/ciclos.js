52a60d0a527f5794bbdc21bd8815c851e6e91475
torcoroma <torcoromaarias@gmail.com>
2023-03-14 17:16:23 -0500

    Ejercicio 1 de de ciclos

---
+++ js/ciclos.js
@@ -0,0 +1,9 @@
+// CICLOS EJERCICIO 1
+// Ejercicios Ciclos
+// Un bucle while que cuente desde 50 hasta 40 (fíjate que en este caso es decreciente)
+
+let nu = 50;
+while (nu >= 40) {
+  console.log(` numeros ${nu}`);
+  nu--;
+}
