382da736f11f890a2366bb82ac0cff00d1f847d0
torcoroma <torcoromaarias@gmail.com>
2023-03-17 17:48:13 -0500

    taps listo, functon 3

--- js/functiones.js
+++ js/functiones.js
@@ -25,4 +25,52 @@ const arismetic = () => {
   return arr2;
 };
 
-console.log(`Resultado ${arismetic()}`);
+// console.log(`Resultado ${arismetic()}`);
+
+//EJERCICIO 3
+// Hacer un programa que muestre un menú con las opciones sumar, restar, multiplicar y dividir,
+// el programa solicitará una opción y realizará la tarea elegida, se debe usar procedimientos.
+
+const boton = document.getElementsByClassName("boton");
+const cuerpo = document.getElementById("cuerpo");
+const tapsBody = document.getElementsByClassName("tap");
+const taps1 = document.getElementById("tap1");
+const taps2 = document.getElementById("tap2");
+const taps3 = document.getElementById("tap3");
+const taps4 = document.getElementById("tap4");
+// console.log(taps);
+console.log(boton);
+const menu1 = () => {
+  tapsBody[0].style.display = "block";
+  tapsBody[1].style.display = "none";
+  tapsBody[2].style.display = "none";
+  tapsBody[3].style.display = "none";
+};
+
+const menu2 = () => {
+  tapsBody[0].style.display = "none";
+  tapsBody[1].style.display = "block";
+  tapsBody[2].style.display = "none";
+  tapsBody[3].style.display = "none";
+  
+};
+
+const menu3 = () => {
+  tapsBody[0].style.display = "none";
+  tapsBody[1].style.display = "none";
+  tapsBody[2].style.display = "block";
+  tapsBody[3].style.display = "none";
+};
+
+const menu4 = () => {
+  tapsBody[0].style.display = "none";
+  tapsBody[1].style.display = "none";
+  tapsBody[2].style.display = "none";
+  tapsBody[3].style.display = "block";
+};
+
+// cuerpo.addEventListener("click", menu);
+taps1.addEventListener("click", menu1);
+taps2.addEventListener("click", menu2);
+taps3.addEventListener("click", menu3);
+taps4.addEventListener("click", menu4);
