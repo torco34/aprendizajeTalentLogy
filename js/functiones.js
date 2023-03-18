01bd54f099339cfff5375e931e39af76eca8337c
torcoroma <torcoromaarias@gmail.com>
2023-03-18 15:21:26 -0500

    Ejercicio 4 funciones

--- js/functiones.js
+++ js/functiones.js
@@ -90,7 +90,6 @@ const multiplicar = () => {
 // multiplicar();
 
 const divicion = () => {
-  
   let num1 = 21;
   let num2 = 1;
   if (num1 < num2) {
@@ -105,4 +104,47 @@ const divicion = () => {
 // divicion();
 
 //EJERCICIO 4
+// Hacer un programa que pida por pantalla un número del 1 al 10 y mediante
+// un procedimiento muestre por pantalla el número escrito en letras.
 
+const numeroLetra = () => {
+  let num = parseInt(prompt("ingresar numero"));
+  switch (num) {
+    case 1:
+      console.log(`Uno `);
+      break;
+    case 2:
+      console.log(`Dos`);
+      break;
+    case 3:
+      console.log(`Tres`);
+      break;
+
+    case 4:
+      console.log(`Cuatro`);
+      break;
+    case 5:
+      console.log(`Cinco`);
+      break;
+    case 6:
+      console.log(`Seis`);
+      break;
+    case 7:
+      console.log(`Siete`);
+      break;
+    case 8:
+      console.log(`Ocho`);
+      break;
+    case 9:
+      console.log(`Nueve`);
+      break;
+    case 10:
+      console.log(`Diez`);
+      break;
+    default:
+      "";
+      console.log(`Numeró no evaluado`);
+      break;
+  }
+};
+numeroLetra();
