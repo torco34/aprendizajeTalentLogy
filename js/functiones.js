6eefb0bb80f8a834efc338380daaeb17e4e062a5
torcoroma <torcoromaarias@gmail.com>
2023-03-22 15:59:54 -0500

    renderizado en pantalla

--- js/functiones.js
+++ js/functiones.js
@@ -50,104 +50,122 @@ const taps = (e) => {
   taps4.style.display = "none";
   const link = e.target;
   const cuerpoId = link.dataset.cuerpo;
-  console.log(cuerpoId);
   const cuerpo = document.querySelector(cuerpoId);
   cuerpo.style.display = "block";
   const papa = link.parentNode;
   papa.classList.add("active");
 };
+// funcion de sumar
 const sumaInput = (e) => {
   e.preventDefault();
-  // const input1 = document.getElementById("input1");
+  const input1 = document.getElementById("input1");
   const input2 = document.getElementById("input2");
   const element = document.getElementById("element");
   let suma1 = parseInt(input1.value);
   let suma2 = parseInt(input2.value);
   total = suma1 + suma2;
-  console.log(total);
-  if (total === 0) {
-    element.innerHTML = `Suma ${total}`;
-  }
-  element.innerHTML = `Suma ${total}`;
+  element.innerHTML = ` suma es ${total}`;
 };
 
 cuerpo.addEventListener("click", taps);
 suma.addEventListener("click", sumaInput);
 // fuccion de del input sumar
-const restar = () => {
-  let resta1 = 45;
-  let resta2 = 6;
-  resultado = resta1 - resta2;
+// FUNCION DE RESTA
+const restaBoton = document.getElementById("resta");
+const restar = (e) => {
+  e.preventDefault();
+  const resta1 = document.getElementById("resta1");
+  const resta2 = document.getElementById("resta2");
+  const element = document.getElementById("elementRest");
+  let input1 = parseInt(resta1.value);
+  let input2 = parseInt(resta2.value);
+  resultado = input1 - input2;
   console.log(resultado);
+  element.innerHTML = ` suma es ${resultado}`;
 };
-// restar();
-const multiplicar = () => {
-  let num = 7;
-  let num2 = 8;
-  multip = num * num2;
-  console.log(multip);
+restaBoton.addEventListener("click", restar);
+// TERMINA FUNCION DE RESTAS
+// MULTIPLICACIÓN EJERCICIO
+const multBoton = document.getElementById("multButon");
+const multiplicar = (e) => {
+  e.preventDefault();
+  const inputMult1 = document.getElementById("inputMult1");
+  const inputMult2 = document.getElementById("inputMult2");
+  const elementMult = document.getElementById("elementMult");
+  let input1 = parseInt(inputMult1.value);
+  let input2 = parseInt(inputMult2.value);
+  multip = input1 * input2;
+  elementMult.innerHTML = ` La multiplicación es ${multip}`;
 };
+multBoton.addEventListener("click", multiplicar);
 // multiplicar();
-
-const divicion = () => {
-  let num1 = 21;
-  let num2 = 1;
-  if (num1 < num2) {
-    divic = num2 / num1;
-    console.log(divic);
-  } else {
-    divic = num1 / num2;
-    console.log(divic);
-  }
+// DIVICION EJERCICIO
+const diviBoton = document.getElementById("diviBoton");
+const divicion = (e) => {
+  e.preventDefault();
+  const divici1 = document.getElementById("divici1");
+  const divici2 = document.getElementById("divici2");
+  const elementDivici = document.getElementById("elementDiv");
+  let inputDi = parseInt(divici1.value);
+  let inputDiv = parseInt(divici2.value);
+  resultado = inputDi / inputDiv;
+  elementDivici.innerHTML = ` Resultado  ${resultado}`;
 };
-
-// divicion();
+diviBoton.addEventListener("click", divicion);
 
 //EJERCICIO 4
 // Hacer un programa que pida por pantalla un número del 1 al 10 y mediante
 // un procedimiento muestre por pantalla el número escrito en letras.
-
-const numeroLetra = () => {
-  let num = parseInt(prompt("ingresar numero"));
+const button = document.getElementById("boton");
+const numeroLetra = (e) => {
+  e.preventDefault();
+  const element = document.getElementById("elemento");
+  let num = parseInt(prompt(""));
+  console.log(num);
   switch (num) {
     case 1:
-      console.log(`Uno `);
+      element.innerHTML = ` Uno`;
+
       break;
     case 2:
-      console.log(`Dos`);
+      element.innerHTML = ` Dos`;
       break;
     case 3:
-      console.log(`Tres`);
+      element.innerHTML = ` Tres`;
       break;
 
     case 4:
-      console.log(`Cuatro`);
+      element.innerHTML = ` Cuatro`;
+
       break;
     case 5:
-      console.log(`Cinco`);
+      element.innerHTML = ` Cinco`;
       break;
     case 6:
-      console.log(`Seis`);
+      element.innerHTML = `Seis`;
       break;
     case 7:
-      console.log(`Siete`);
+      element.innerHTML = ` Siete`;
+
       break;
     case 8:
-      console.log(`Ocho`);
+      element.innerHTML = `Ocho`;
+
       break;
     case 9:
-      console.log(`Nueve`);
+      element.innerHTML = ` Nueve`;
       break;
     case 10:
-      console.log(`Diez`);
+      element.innerHTML = ` Diez`;
+
       break;
     default:
-      "";
-      console.log(`Numeró no evaluado`);
+      element.innerHTML = `Numeros no validos`;
+
       break;
   }
 };
-
+button.addEventListener("click", numeroLetra);
 // numeroLetra();
 
 // EJERCICIO 5
@@ -167,11 +185,11 @@ const celsiusAKelvin = (celsiusK) => {
 
 let celsiusF = 32;
 let resultado = celsiusAFahrenheit(celsiusF);
-console.log(` Cº ${celsiusF} = Fº${resultado} `);
+// console.log(` Cº ${celsiusF} = Fº${resultado} `);
 
 let celsiusK = 31;
 const gradoKelvin = celsiusAKelvin(celsiusK);
-console.log(` Cº ${celsiusK} = ${gradoKelvin} Kº `);
+// console.log(` Cº ${celsiusK} = ${gradoKelvin} Kº `);
 
 // EJERCICIO 6
 // Hacer un programa que muestre una tabla de multiplicar hasta el 20
@@ -201,7 +219,7 @@ const ordenNum = () => {
   let ordenMay = Math.max(num1, num2, num3);
 
   let ordenMen = num1 + num2 + num3 - ordenMin - ordenMay;
-  console.log(ordenMin, ordenMen, ordenMay);
+  // console.log(ordenMin, ordenMen, ordenMay);
 };
 
 // ordenNum();
@@ -222,8 +240,8 @@ const fibonacci2 = () => {
   let orden1 = Math.min(numero1, numero2, numero3);
   let orden3 = Math.max(numero1, numero2, numero3);
   let orden2 = numero1 + numero2 + numero3 - orden1 - orden3;
-  console.log(orden3, orden2, orden1);
-  console.log(numero1, numero2, numero3);
+  // console.log(orden3, orden2, orden1);
+  // console.log(numero1, numero2, numero3);
 };
 fibonacci2();
 
@@ -241,7 +259,7 @@ const sumaN = () => {
   let orden3 = Math.max(suma, suma2, suma3);
   let orden1 = Math.min(suma, suma2, suma3);
   let orden2 = suma + suma2 + suma3 - orden1 - orden3;
-  console.log(orden3, orden2, orden1);
+  // console.log(orden3, orden2, orden1);
 };
 
 // sumaN();
@@ -255,29 +273,65 @@ const binario = (num) => {
   arr.push(num);
   return parseInt(arr.reverse().join(""));
 };
-console.log(binario(10));
-console.log(binario(11));
-console.log(binario(13));
+// console.log(binario(10));
+// console.log(binario(11));
+// console.log(binario(13));
 
 const hola = () => {
-  console.log("Hola");
+  // console.log("Hola");
 };
 
 const mundo = () => {
   hola();
-  console.log("mundo");
+  // console.log("mundo");
 };
 
-mundo();
+// mundo();
 
-const numero2 = (num) => {
-  num = num.toString();
+// const numero2 = (num) => {
+//   let arr2 = [];
+//   num = num.toString();
+//   console.log(num, "num");
+//   let arr = num.split("");
+//   let reverse = arr.reverse();
+//   console.log(reverse, "reverse");
+//   let result = reverse.join("");
+//   arr2 = arr2 + result;
+//   console.log(arr2, "reverse---");
+
+//   return arr2;
+// };
+// console.log(numero2(254));
+
+function invertirNumero(num) {
   console.log(num);
-  let arr = num.split("");
-  let reverse = arr.reverse();
-  let result = reverse.join("");
-  console.log(result);
-  if (arr != "") return arr;
-  return numero2(result);
+  if (num < 10) {
+    console.log(`entra si tine un numero`);
+    return num;
+  } else {
+    const ultimoDigito = num % 10;
+    console.log(ultimoDigito, `ultimo numero`);
+    const resto = Math.floor(num / 10);
+    console.log(resto, `floor son restos ejercicio 1`);
+    return (
+      invertirNumero(ultimoDigito).toString() + invertirNumero(resto).toString()
+    );
+  }
+}
+console.log(invertirNumero(421));
+// console.log(invertirNumero(5));
+
+const math = (n) => {
+  console.log(n, "numero entrante");
+  if (n < 10) {
+    console.log(`retorna cuando sea un dijito`);
+    return n;
+  }
+  const ultimo = n % 10;
+  console.log(ultimo, "ultimo dijito con %");
+  const resto = Math.floor(n / 10);
+  console.log(num, "dividido 2 con floor");
+  return math(ultimo).toString() + math(resto).toString();
 };
-console.log(numero2(345));
+// console.log(math(345));
+// math(23);
