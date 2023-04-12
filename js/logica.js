c0b3d87ae1f87fe274716c44fceb73439bd5641a
torcoroma <torcoromaarias@gmail.com>
2023-04-12 14:47:46 -0500

    css

--- js/logica.js
+++ js/logica.js
@@ -1,9 +1,10 @@
 // Ejercicios de lógica de programación
 
 // primer ejercicio
-const elementOne = document.getElementById("elementOne");
-const btnOne = document.getElementById("btnOne");
+
+const btn1 = document.getElementById("btn1");
 function pedirNumero() {
+  const element1 = document.getElementById("element1");
   const numberOne = parseInt(prompt("Ingresar numero uno"));
   const numberTwo = parseInt(prompt("Ingresar numero dos"));
   addition = numberOne + numberTwo;
@@ -14,31 +15,48 @@ function pedirNumero() {
   console.log(multiplication);
   división = numberOne / numberTwo;
   console.log(división);
-  elementOne.innerHTML = `la suma ${addition} la resta ${rest} la multiplicacion ${multiplication} la division ${división} los numero ingresados    ${numberOne} ,${numberTwo}`;
+  element1.innerHTML = `la suma ${addition} la resta ${rest} la multiplicacion ${multiplication} la division ${división} los numero ingresados    ${numberOne} ,${numberTwo}`;
 }
 
-btnOne.addEventListener("click", pedirNumero);
+btn1.addEventListener("click", pedirNumero);
 
 //  2 EJERCICIO DE LOGICA
-const celsius = 32;
-function convertirFahrenheitACelsius(celsius) {
-  let resultado = (celsius * 9) / 5.0 + 32;
-  return resultado;
-}
-function convertirCelsiusAFahrenheit(fahrenheit) {
-  let resultado = fahrenheit + (32 / 5.0) * 9;
-  return resultado;
-}
+// const celsius = 32;
+// function convertirFahrenheitACelsius(celsius) {
+//   let resultado = (celsius * 9) / 5.0 + 32;
+//   return resultado;
+// }
+// function convertirCelsiusAFahrenheit(fahrenheit) {
+//   let resultado = fahrenheit + (32 / 5.0) * 9;
+//   return resultado;
+// }
 
-let resultado = convertirCelsiusAFahrenheit(celsius);
+// let resultado = convertirCelsiusAFahrenheit(celsius);
 
-console.log(`resultado de ${celsius} 0C es = ${resultado} 0F`);
+// console.log(`resultado de ${celsius} 0C es = ${resultado} 0F`);
 
+btn2.addEventListener("click", () => {
+  const element2 = document.getElementById("element2");
+  console.log("que es");
+  const celsius = parseInt(prompt("Ingresé celsius"));
+  function convertirFahrenheitACelsius(celsius) {
+    let resultado = (celsius * 9) / 5.0 + 32;
+    return resultado;
+  }
+  function convertirCelsiusAFahrenheit(fahrenheit) {
+    let resultado = fahrenheit + (32 / 5.0) * 9;
+    return resultado;
+  }
+
+  let resultado = convertirCelsiusAFahrenheit(celsius);
+  element2.innerHTML = `resultado de ${celsius} 0C es = ${resultado} 0F`;
+});
 // 3 EJERCICIO DE LOGICA
 
 // Sacar la hipotenusa de un triángulo rectángulo, pidiendo al usuario el valor de los 2 catetos.
-const catetos = document.getElementById("catetos");
+const btn3 = document.getElementById("btn3");
 function calcularHipotenusa() {
+  const element3 = document.getElementById("element3");
   let cateto1 = prompt("Ingresa cateto 1");
   let cateto2 = prompt("Ingresa catetos 2");
   let a = Math.pow(cateto1, 2);
@@ -46,119 +64,140 @@ function calcularHipotenusa() {
   suma = a + b;
 
   resultado = Math.sqrt(Math.pow(cateto1, 2) + Math.pow(cateto2, 2));
-  console.log(
-    `Resultado: de numeros ingresados ${cateto1} y ${cateto2}, resultado elevados  es ${a} y ${b},  la hiputenusa de ${suma} es ${resultado}`
-  );
+
+  element3.innerHTML = `Resultado: de numeros ingresados ${cateto1} y ${cateto2}, resultado elevados  es ${a} y ${b},  la hiputenusa de ${suma} es ${resultado}`;
 }
 
-catetos.addEventListener("click", calcularHipotenusa);
+btn3.addEventListener("click", calcularHipotenusa);
 
 //  EJERCICIO 4 DE LOGICA
 // Hacer un Programa que calcule longitudes de Circunferencia.
 
-function circunferencia(radio) {
-  let circulo = 2 * Math.PI * radio;
-  console.log(`Circuferencia ${circulo}`);
-}
-function longitud(radio) {
-  let area = Math.PI * Math.pow(radio, 2);
-  console.log(`longitud ${area}`);
-}
-longitud(4);
-circunferencia(4);
+const btn4 = document.getElementById("btn4");
+
+btn4.addEventListener("click", () => {
+  const element4 = document.getElementById("element4");
+
+  function circunferencia() {
+    const radio = parseInt(prompt("ingresa rado"));
+    let circulo = 2 * Math.PI * radio;
+    return circulo;
+  }
+  function longitud() {
+    const radio = parseInt(prompt("ingresa rado"));
+    let area = Math.PI * Math.pow(radio, 2);
+    return area;
+  }
+  let frecuencia = circunferencia();
+  let longit = longitud();
+  element4.innerHTML = ` Circuferencia ${frecuencia}  Longitud ${longit} `;
+});
 // 5 EJERCICIO DE LOGICA
 //  Hacer un Programa que calcule áreas de trapecios.
-const btnTrapecio = document.getElementById("btnTrapecio");
-function calcularAreaTrapecio() {
+const btn = document.getElementById("btnTrapecio");
+
+btn5.addEventListener("click", () => {
+  const element5 = document.getElementById("element5");
   let numeroMax = prompt("Ingresa numero mas grade");
   let numeroMin = prompt("Ingresa numero mas pequeno");
   let altura = prompt("Ingresa altura");
 
   let area = (parseInt(numeroMax) + parseInt(numeroMin) / 2) * parseInt(altura);
-  // console.log(`El area del trapecio es ${area}`);
-}
-btnTrapecio.addEventListener("click", calcularAreaTrapecio);
+  element5.innerHTML = `El trapecio es: ${area}`;
+});
 
 // 6 EJERCICIO de logica
 
 // Calcule la media aritmética de 3 números cualesquiera.
-const btnArismet = document.getElementById("btnArismet");
+const btn6 = document.getElementById("btn6");
 function calcularMedia() {
+  const element6 = document.getElementById("element6");
   const num1 = parseInt(prompt("num1"));
   const num2 = parseInt(prompt("num2"));
   const num3 = parseInt(prompt("num3"));
   sumaArismetic = num1 + num2 + num3;
   resultadoDivicion = sumaArismetic / 3;
-  // console.log(resultadoDivicion);
+  element6.innerHTML = `La media ${resultadoDivicion}`;
 }
-btnArismet.addEventListener("click", calcularMedia);
+btn6.addEventListener("click", calcularMedia);
 
 // 7 EJERCICIO DE LOGICA
 //  Una tienda ofrece un descuento del 15% sobre el total de la compra y un
 // cliente desea saber cuánto deberá pagar finalmente por su compra.
-function descuento() {
-  let compraSinDescuento = 300;
-  let descuento = 15;
+const btn7 = document.getElementById("btn7");
+
+// function descuento() {
+
+//   // console.log(compraFinal);
+// }
+// descuento();
+btn7.addEventListener("click", () => {
+  const element7 = document.getElementById("element7");
+
+  const compraSinDescuento = parseInt(prompt("Compra sin descuento"));
+  const descuento = parseInt(prompt("descuento de 15%"));
+
   const compraFinal =
     compraSinDescuento - (descuento * compraSinDescuento) / 100;
-  // console.log(compraFinal);
-}
-descuento();
-
+  element7.innerHTML = `Pago final con descuento ${compraFinal}`;
+});
 // 8 EJERCICIO DE LOGICA
 // Dadas las horas trabajadas de una persona y el valor por hora. Calcular su salario e imprimis.
+const btn8 = document.getElementById("btn8");
 
-function calcularSalario() {
+btn8.addEventListener("click", () => {
+  const element8 = document.getElementById("element8");
   let horasTrabajadas = 8;
   let valorHoras = 20;
   let diasMes = 20;
   valorDia = horasTrabajadas * valorHoras;
   salariMes = valorDia * diasMes;
-  // console.log(valorDia);
-  // console.log(salariMes);
-}
-calcularSalario();
-
+  element8.innerHTML = `el valor dia ${valorDia} salario mes ${salariMes}`;
+});
 // 9 EJERCICIO DE LOGICA
 
-// Calcular el nuevo salario de un obrero, si obtuvo un incremento del 25% sobre su salario anterior.
+// Calcular el nuevo salario de un obrero, si obtuvo un incremento del 25%
+// sobre su salario anterior.
+const btn9 = document.getElementById("btn9");
 
-function calcularNuevoSalaro(salario, incremento, nuevoSalario) {
+btn9.addEventListener("click", () => {
+  const element9 = document.getElementById("element9");
+  let salario = parseInt(prompt("Salario anterior"));
   incremento = salario * 0.25;
   nuevoSalario = salario + incremento;
-  // console.log(incremento);
-  // console.log(nuevoSalario);
-}
-calcularNuevoSalaro(200);
 
+  element9.innerHTML = `El nuevo salario es:${nuevoSalario}`;
+});
 // 10 EJERCICIO DE LOGICA
 // Comprobar a través de un programa si un alumno aprobó o no un examen (Aprueba si su nota es mayor a 10.5).
-const btnNota = document.getElementById("btnNota");
+const btn10 = document.getElementById("btn10");
 
-function knowNote() {
+btn10.addEventListener("click", () => {
+  const element10 = document.getElementById("element10");
   let noteFinish = prompt("Ingresar nota");
   if (noteFinish > 10.5) {
     console.log("Aprobó");
+    element10.innerHTML = `aprobó con la nota ${noteFinish}`;
   } else {
-    console.log("Reaprobo");
+    element10.innerHTML = `reprobó con la nota ${noteFinish}`;
   }
-}
-
-btnNota.addEventListener("click", knowNote);
+});
+// btnNota.addEventListener("click", knowNote);
 // 11 EJERCICIO DE LOGICA
 // Comprobar si un número digitado por el usuario es positivo o negativo.
-const btnPositive = document.getElementById("btnPositive");
+const btn11 = document.getElementById("btn11");
 
 function knowNumber() {
+  const element11 = document.getElementById("element11");
   const numero = prompt("ingresar numero");
   if (numero >= 0) {
-    console.log(` El numero ${numero}, es positivo`);
+    element11.innerHTML = `El numero ${numero}, es positivo`;
   } else {
-    console.log(`El numero ${numero}, es negativo`);
+    element11.innerHTML = `El numero ${numero}, es negativo`;
   }
 }
 
-btnPositive.addEventListener("click", knowNumber);
+btn11.addEventListener("click", knowNumber);
 // 12 EJERCICIO DE LOGICA
 // Visualizar la tarifa de la luz según el gasto de corriente
 //  eléctrica. Para un gasto  menor de 1.000Kwxh la tarifa es 1.2,
@@ -166,120 +205,130 @@ btnPositive.addEventListener("click", knowNumber);
 
 // DATOS
 
-const btnTarifa = document.getElementById("btnTarifa");
+const btn12 = document.getElementById("btn12");
 function knowTafifa() {
+  const element12 = document.getElementById("element12");
   let tarifa1 = 1.2;
   let tarifa2 = 1.0;
   let tarifa3 = 0.9;
   const gasto = prompt("Ingresar gasto eletrico");
   if (gasto < 1000) {
     console.log(tarifa1);
+    element12.innerHTML = ` es:${tarifa1}`;
   } else if (gasto > 1000 && gasto < 1800) {
     console.log(tarifa2);
+    element12.innerHTML = ` es:${tarifa2}`;
   } else {
     console.log(tarifa3);
+    element12.innerHTML = ` es:${tarifa3}`;
   }
 }
 
-btnTarifa.addEventListener("click", knowTafifa);
+btn12.addEventListener("click", knowTafifa);
 // 13 EJERCICIO DE LOGICA
 // Determinar si un número es par, impar.
 
-const btnPar = document.getElementById("btnPar");
+const btn13 = document.getElementById("btn13");
 function knowIfPar() {
   let numeros = prompt("Ingresar numero");
-
+  const element13 = document.getElementById("element13");
   if (numeros % 2 === 0) {
-    console.log(`El ${numeros} es par`);
+    element13.innerHTML = `El ${numeros} es par`;
   } else {
-    console.log(`El ${numeros} es impar`);
+    element13.innerHTML = `El ${numeros} es impar`;
   }
 }
 
-btnPar.addEventListener("click", knowIfPar);
+btn13.addEventListener("click", knowIfPar);
 // 14 EJERCICIO DE LOGICA
 // Calcular el mayor de dos números leídos del teclado y visualizarlo en pantalla
-const btnCalcular = document.getElementById("btnCalcular");
+const btn14 = document.getElementById("btn14");
 function calcularDosNumeros() {
+  const element14 = document.getElementById("element14");
   const numero1 = prompt("Ingresar numero 1");
   const numero2 = prompt("Ingresar numero 2");
   if (numero1 > numero2) {
-    console.log(`numero ${numero1} es mayor`);
+    element14.innerHTML = `numero ${numero1} es mayor`;
   } else {
     console.log(`El numero es ${numero2} Mayor`);
+    element14.innerHTML = `El numero es ${numero2} Mayor`;
   }
 }
-btnCalcular.addEventListener("click", calcularDosNumeros);
+btn14.addEventListener("click", calcularDosNumeros);
 
 // 15 EJERCICIO DE  LOGICA
 // Ingrese un número y calcule e imprima su raíz cuadrada.
 // Si el número es negativo  imprima el número y un mensaje
 // que diga “tiene raíz imaginaria”.
-const btnRaiz = document.getElementById("btnRaiz");
+const btn15 = document.getElementById("btn15");
 function imprimaRaizCuadrada() {
-  const numeroIngresado = prompt("ingresar par ver raiz");
+  const element15 = document.getElementById("element15");
+  const numeroIngresado = prompt("ingresar para ver raíz");
   let numeroNegetive = 0;
 
   if (numeroIngresado > numeroNegetive) {
-    console.log(Math.sqrt(numeroIngresado));
+    let siRaiz = Math.sqrt(numeroIngresado);
+    console.log();
+    element15.innerHTML = `la raíz es: ${siRaiz}`;
   } else {
-    console.log(`El numero ${numeroIngresado} tine raiz imaginaria`);
+    element15.innerHTML = `El numero ${numeroIngresado} tine raíz imaginaria`;
   }
 }
 
-btnRaiz.addEventListener("click", imprimaRaizCuadrada);
+btn15.addEventListener("click", imprimaRaizCuadrada);
 // 16 EJERCICIO DE LOGICA
 // Ingresar por teclado el nombre y el signo de cualquier
 // persona e imprima, el nombre sólo si la persona es signo
 // Aries, caso contrario imprima no es signo Aries.
-const btnSigno = document.getElementById("btnSigno");
+const btn16 = document.getElementById("btn16");
 
 function imprimaSigno() {
+  const element16 = document.getElementById("element16");
   const nombre = prompt("Ingresa nombre");
   const signo = prompt("Ingresa Signo");
   let miSigno = "aries";
 
   if (signo === miSigno) {
-    console.log(`${nombre} tu signo es ${signo}`);
+    element16.innerHTML = `${nombre} tu signo es ${signo}`;
   } else {
-    console.log("No es signo Aries");
+    element16.innerHTML = `No es signo Aries`;
   }
 }
 
-btnSigno.addEventListener("click", imprimaSigno);
+btn16.addEventListener("click", imprimaSigno);
 
 // EJERCICIO 17 DE LOGICA
 
 // Ingresar por teclado el nombre, la edad y el sexo de cualquier persona e
 // imprima, solo si la persona es de sexo masculino y mayor de edad, el
 // nombre de la persona.
-const btnEdad = document.getElementById("btnEdad");
+const btn17 = document.getElementById("btn17");
 function nombreEdad() {
+  const element17 = document.getElementById("element17");
   let nombre = prompt(" Ingreso nombre");
   let edadIngresada = prompt("ingresar edad");
   let sexoIngresado = prompt("ingresar el sexo");
 
   let edadMayor = 18;
 
-  let sexo2 = "masculino";
+  let sexo2 = "Masculino";
   if (edadIngresada >= edadMayor && sexoIngresado === sexo2) {
-    console.log(
-      `Hola ${nombre} tu eded es ${edadIngresada}  y eres ${sexoIngresado}`
-    );
+    element17.innerHTML = `Hola ${nombre} tu eded es ${edadIngresada}  y eres ${sexoIngresado}`;
   } else {
-    console.log(`Hola ${nombre} no calificas`);
+    element17.innerHTML = `Hola ${nombre} no calificas`;
   }
 }
 
-btnEdad.addEventListener("click", nombreEdad);
+btn17.addEventListener("click", nombreEdad);
 // EJERCICIO 18  DE LOGICA
 
 // Una distribuidora de motocicletas tiene una promoción de fin de año qué consiste
 // en lo siguiente. Las motos marca Honda tienen un descuento del 5%, las marcas Yamaha
 // del 8% y las Suzuki del 10%, las otras marcas 2%
 
-const btnDescuento = document.getElementById("btnDescuento");
+const btn18 = document.getElementById("btn18");
 function motocicletas() {
+  const element18 = document.getElementById("element18");
   let precioBruto = parseInt(prompt("ingresar precio"));
 
   let descuentoHonda = 5;
@@ -302,25 +351,24 @@ function motocicletas() {
   let marca = prompt("ingresar marca");
   switch (marca) {
     case "honda":
-      console.log(`El precio de la Honda con descuento ${precioDescuento}`);
-
+      element18.innerHTML = `El precio de la Honda con descuento ${precioDescuento}`;
       break;
     case "yamaha":
-      console.log(
-        ` El precio de la Yamaha con descuento ${precioDescuentoYama}`
-      );
+      element18.innerHTML = ` El precio de la Yamaha con descuento ${precioDescuentoYama}`;
+
       break;
     case "suzuki":
-      console.log(
-        `El precio de la Suzuki con descuento ${precioDescuentoSuzuki}`
-      );
+      element18.innerHTML = `El precio de la Suzuki con descuento ${precioDescuentoSuzuki}`;
+
       break;
     case "otras":
-      console.log(`Otras marcas ${precioDescuentoOtros}`);
+      element18.innerHTML = `Otras marcas ${precioDescuentoOtros}`;
+
       break;
     default:
+      element18.innerHTML = `Los datos no son valido ${precioDescuentoOtros}`;
       break;
   }
 }
 
-btnDescuento.addEventListener("click", motocicletas);
+btn18.addEventListener("click", motocicletas);
--- js/main.js
+++ js/main.js
@@ -10,80 +10,87 @@ function viewNumber(e) {
 }
 btnNumber.addEventListener("click", viewNumber);
 
-// SEGUNDO EJERCICIO introducido 
-const inputProducts = document.getElementById("products");
-const inputDiscount = document.getElementById("discount");
-const elementDos = document.getElementById("elementDos");
-const btn = document.getElementById("btn");
-console.log(inputProducts);
+// SEGUNDO EJERCICIO introducido
+
+const btn2 = document.getElementById("btn2");
+
 function discountPrice(e) {
   e.preventDefault();
-  let valueProduct = inputProducts.value;
+  const element2 = document.getElementById("element2");
+  const inputProduct = document.getElementById("products2");
+  const inputDiscount = document.getElementById("discount2");
+  let valueProduct = inputProduct.value;
   let discountProduct = inputDiscount.value;
   const preciTotal = Math.abs(
     (valueProduct * discountProduct) / 100 - valueProduct
   );
-  elementDos.innerHTML = `El precio es de 
+  element2.innerHTML = `El precio es de 
   ${valueProduct} tiene un descuento de 
   ${discountProduct}%  el precio final con el descuento 
   ${preciTotal}`;
+  inputProduct.value = "";
+  inputDiscount.value = "";
 }
 
-btn.addEventListener("click", discountPrice);
+btn2.addEventListener("click", discountPrice);
 
 // TERCER EJERCICIO
 
-const button = document.getElementById("button");
-const input = document.getElementById("input");
-const elementTotal = document.getElementById("elemenTotal");
-
+const button3 = document.getElementById("button3");
 function calculateChange(e) {
   e.preventDefault();
-  let dollar = input.value;
+  const element3 = document.getElementById("element3");
+  const input = document.getElementById("input3");
+  dollar = input.value;
   let valueEnd = dollar / 1.3325;
   valueEnd = valueEnd.toFixed(2);
-  elementTotal.innerHTML = `Este es el resultado en Euro ${valueEnd}`;
+  element3.innerHTML = `Este es el resultado en Euro ${valueEnd}`;
+  input.value = "";
 }
 
-button.addEventListener("click", calculateChange);
+button3.addEventListener("click", calculateChange);
 
 // CUARTO EJERCICIO
 
-const numeros = document.getElementsByClassName("num1");
-const btnMedia = document.getElementById("btnMedia");
-const elementMedia = document.getElementById("elementMedia");
+const btn4 = document.getElementById("btn4");
 
 function imprimirMedia(e) {
   e.preventDefault();
+  const numeros = document.getElementsByClassName("num1");
+  const element4 = document.getElementById("element4");
   let sum1 = parseInt(numeros[0].value);
   let sum2 = parseInt(numeros[1].value);
   let sum3 = parseInt(numeros[2].value);
   let sum4 = parseInt(numeros[3].value);
-
   const sumaTotal = sum1 + sum2 + sum3 + sum4;
   let media = sumaTotal / 4;
 
-  elementMedia.innerHTML = `La media es la ${media}, la suma de los cuatro numero es ${sumaTotal}`;
+  element4.innerHTML = `La media es la ${media}, la suma de los cuatro numero es ${sumaTotal}`;
+  numeros[0].value = "";
+  numeros[1].value = "";
+  numeros[2].value = "";
+  numeros[3].value = "";
 }
 
-btnMedia.addEventListener("click", imprimirMedia);
+btn4.addEventListener("click", imprimirMedia);
 
 //  QUINTO EJERCICIO
-const resistencia1 = document.getElementById("resistencia1");
-const resistencia2 = document.getElementById("resistencia2");
-const elementReq = document.getElementById("elementReq");
-const btnResistes = document.getElementById("btnResist");
+
+const btn5 = document.getElementById("btn5");
 
 function sumar(e) {
   e.preventDefault();
-
+  const resistencia1 = document.getElementById("resistencia1");
+  const resistencia2 = document.getElementById("resistencia2");
+  const element5 = document.getElementById("element5");
   let valor1 = parseInt(resistencia1.value);
   let valor2 = parseInt(resistencia2.value);
   let suma = valor1 + valor2;
-  let multiplicacion = valor1 * valor2;
+  let multiplicación = valor1 * valor2;
+  let resultadoFinal = multiplicación / suma;
+  element5.innerHTML = ` la multiplication ${multiplicación} la suma es ${suma},  el resultado de resistencia equivalente es ${resultadoFinal}`;
 
-  let resultadoFinal = multiplicacion / suma;
-
-  elementReq.innerHTML = ` la multiplication ${multiplicacion} la suma es ${suma},  el resultado de resistencia equivalente es ${resultadoFinal}`;
+  resistencia1.value = "";
+  resistencia2.value = "";
 }
-btnResistes.addEventListener("click", sumar);
+btn5.addEventListener("click", sumar);
